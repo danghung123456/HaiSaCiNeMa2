e9fda2fe5effc0bb54b8b250f4481442855016dd
ldt15420 <ldt15420@gmail.com>
2020-10-08 11:30:52 +0700

    Tài: push bài của Bình

---
+++ src/main/java/com/Entity/Movie.java
@@ -0,0 +1,115 @@
+package com.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="movie")
+public class Movie {
+		
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="movieid")
+    private Integer movieId;
+    
+    @Column(name = "moviename")
+    private String movieName;
+    
+    @Column(name="duration")
+    private String duration;
+    
+    @Column(name="description")
+    private String description;
+    
+    @Column(name="thumbnail")
+    private String thumbnail;
+    
+    @Column(name="actors")
+    private String actors;
+    
+    @Column(name="director")
+    private String director;
+    
+    @Column(name = "agelimit")
+    private Integer ageLimit;
+    
+    @Column(name="status")
+    private Integer status;
+
+    
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getActors() {
+		return actors;
+	}
+
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public Integer getAgeLimit() {
+		return ageLimit;
+	}
+
+	public void setAgeLimit(Integer ageLimit) {
+		this.ageLimit = ageLimit;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+}
