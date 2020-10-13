0f63ca72b68595a058d79608a8e9cc6f5954d412
hunghenry1995 <hunghenry1995@gmail.com>
2020-10-13 09:18:47 +0700

    sd

--- src/main/java/com/Repository/RoomRepository.java
+++ src/main/java/com/Repository/RoomRepository.java
@@ -15,19 +15,19 @@ import com.Entity.Room;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 
-<<<<<<< HEAD
+
 //	@Query("FROM Room")
 //	Page<Room> findAll(Pageable pageable);
 //	
 //	@Query("FROM Room r WHERE r.roomID= :id")
 //	Optional<Room> findById(Integer id);
-=======
+
 	@Query("FROM Room")
 	Page<Room> findAll(Pageable pageable);
 	
 	@Query("FROM Room r WHERE r.roomId= :id")
 	Optional<Room> findById(Integer id);
->>>>>>> 77eea5eb881e7a6d007a5a66397687eda02c6c8f
+
 	
 	@Query(value = "SELECT * FROM Room WHERE r.roomName LIKE %?% ", nativeQuery = true)
 	List<Room> findByName(String name);
