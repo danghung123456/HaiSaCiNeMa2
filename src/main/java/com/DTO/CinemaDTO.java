80c0b3e6fc36bac644da0a2422af2fd7a93f7e32
hunghenry1995 <hunghenry1995@gmail.com>
2020-10-31 09:49:16 +0700

    test

--- src/main/java/com/DTO/CinemaDTO.java
+++ src/main/java/com/DTO/CinemaDTO.java
@@ -17,6 +17,7 @@ public class CinemaDTO {
 		this.cinemaId = cinemaId;
 		this.name = name;
 		this.address = address;
+		System.out.println("sadasdasdsadsadassd");
 	}
 
 	public boolean isNull(boolean includeId) {
