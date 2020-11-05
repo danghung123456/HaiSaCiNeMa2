6263154318efdb080a8b79491de1b38af67f86a7
Binh <huynhhuubinh0az@gmail.com>
2020-11-05 16:38:29 +0700

    code for ticket

--- src/main/java/com/Configuration/ApplicationConfig.java
+++ src/main/java/com/Configuration/ApplicationConfig.java
@@ -18,7 +18,7 @@ public class ApplicationConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void	addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:8080/");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "DELETE", "GET", "POST");
 			}
 		};
 	}
