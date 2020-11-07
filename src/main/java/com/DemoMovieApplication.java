a407d265107b95a317b99efa1f7fa2074ce6a78b
hunghenry1995 <hunghenry1995@gmail.com>
2020-11-07 17:45:30 +0700

    file config

--- src/main/java/com/DemoMovieApplication.java
+++ src/main/java/com/DemoMovieApplication.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoMovieApplication {
-	@Bean
-	public WebMvcConfigurer webMVCConfig() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void	addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer webMVCConfig() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void	addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+//			}
+//		};
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMovieApplication.class, args);
 	}
