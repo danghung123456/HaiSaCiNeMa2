f77daadb8ff91285353cb6af0d72aac8f64361fe
devhaidang <tihaman2000@gmail.com>
2020-11-23 12:20:03 +0700

    update Employee

--- src/main/java/com/Repository/UserRepository.java
+++ src/main/java/com/Repository/UserRepository.java
@@ -4,16 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import org.springframework.stereotype.Repository;
-
 import com.Entity.User;
-import com.Entity.Users;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-	
-	
+
 	Optional<User> findByEmail(String email);
 
 }
