b651725f61de07f334d02f9734357072008a536e
tihaman2000 <tihaman2000@gmail.com>
2020-11-23 22:38:17 +0700

    no message

--- src/main/java/com/Configuration/SecurityConfiguration.java
+++ src/main/java/com/Configuration/SecurityConfiguration.java
@@ -48,6 +48,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 }
