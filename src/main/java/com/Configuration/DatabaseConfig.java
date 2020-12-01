6ddea5d9e08f5a6298dacdac2b93ea9fcba44271
hunghenry1995 <hunghenry1995@gmail.com>
2020-12-01 18:51:52 +0700

    a

--- src/main/java/com/Configuration/DatabaseConfig.java
+++ src/main/java/com/Configuration/DatabaseConfig.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
-import javax.sql.DataSource;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+@Component
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 public class DatabaseConfig {
