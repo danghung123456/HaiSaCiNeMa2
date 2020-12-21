db027ee3530f76a27c4c4b7d20c02e6733e4f7ca
hunghenry1995 <hunghenry1995@gmail.com>
2020-12-21 16:51:01 +0700

    config

--- src/main/java/com/Configuration/WebCorsConfig.java
+++ src/main/java/com/Configuration/WebCorsConfig.java
@@ -17,7 +17,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:4200")
+				.allowedOrigins("*")
 				.allowedHeaders("*")
 				.allowedMethods("PUT", "GET", "DELETE", "POST");
 				
